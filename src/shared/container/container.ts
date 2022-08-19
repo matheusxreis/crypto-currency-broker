7ba80f3be83c7e676ac6905f6be6716f975714db
Matheus Reis <matheus.reis081@gmail.com>
2022-08-19 14:33:46 -0300

    feat: added MessageBrokerRepository to tsrynge

--- src/shared/container/container.ts
+++ src/shared/container/container.ts
@@ -1,4 +1,6 @@
-import { CryptoCurrencyRepository } from '../../infra/repositories/CryptoCurrencyRepository';
+import { CryptoCurrencyRepository } from 'src/infra/repositories/CryptoCurrencyRepository';
+import { MessageBrokerRepository } from 'src/infra/repositories/messageBrokerRepository';
 import { container } from 'tsyringe';
 
 container.registerSingleton('CryptoCurrencyRepository', CryptoCurrencyRepository);
+container.registerSingleton('MessageBrokerRepository', MessageBrokerRepository);
