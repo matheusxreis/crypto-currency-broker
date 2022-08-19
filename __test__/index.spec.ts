aea6e0d2fcf4b3408814760466dc7e186a5d4c9b
Matheus Reis <matheus.reis081@gmail.com>
2022-08-19 09:04:32 -0300

    refactor: removing unnecessary tests

--- __test__/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { sum, isUppercase } from '../src';
-
-it('should sum two differents number', () => {
-  expect(sum(6, 8)).toBe(14);
-});
-
-it('should verify if a word is on uppercase correctly', () => {
-  expect(isUppercase('hey')).toBe('The word is not on uppercase... =(');
-});
