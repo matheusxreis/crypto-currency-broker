2d2cf8ef264939d3afac3cd81fd13a7733b1ae7b
Matheus Reis <matheus.reis081@gmail.com>
2022-08-19 10:33:39 -0300

    feat: setted up tsyringe and testing function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import 'reflect-metadata';
+import './shared/container/container.ts';
 import { config as loadEnvVariables } from 'dotenv';
+import { container } from 'tsyringe';
+import { GetCryptoCurrencyValueUseCase } from './data/useCases/getCryptoCurrencyValueUseCase';
 loadEnvVariables();
+
+const getCryptoCurrencyValue = container.resolve(GetCryptoCurrencyValueUseCase);
+
+const start = async () => {
+  const x = await getCryptoCurrencyValue.execute({ currency: 'brl', cryptoCurrency: 'bitcoin' });
+  console.log(x);
+};
+
+start();
