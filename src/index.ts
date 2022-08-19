7413d733b6df44d5193b7a260490bd1847f76ec1
Matheus Reis <matheus.reis081@gmail.com>
2022-08-19 11:16:16 -0300

    feat: init function created

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,28 @@ import './shared/container/container.ts';
 import { config as loadEnvVariables } from 'dotenv';
 import { container } from 'tsyringe';
 import { GetCryptoCurrencyValueUseCase } from './data/useCases/getCryptoCurrencyValueUseCase';
+import { Interval } from './shared/enums/interval';
 loadEnvVariables();
 
 const getCryptoCurrencyValue = container.resolve(GetCryptoCurrencyValueUseCase);
 
-const start = async () => {
-  const x = await getCryptoCurrencyValue.execute({ currency: 'brl', cryptoCurrency: 'bitcoin' });
-  console.log(x);
+const requestBitcoinBRLValue = async () => {
+  return await getCryptoCurrencyValue.execute({ currency: 'brl', cryptoCurrency: 'bitcoin' });
 };
 
-start();
+async function init () {
+  while (true) {
+    const loopTimes = Interval.ONE_MINUTE / Interval.TEN_SECONDS;
+    console.log('STARTING.....');
+    for (let i = 0; i < loopTimes; i++) {
+      const candle = await requestBitcoinBRLValue();
+      console.log(`Consulting....${i + 1} time from ${loopTimes}`);
+
+      console.log(candle);
+      await new Promise(resolve => setTimeout(resolve, Interval.TEN_SECONDS));
+    }
+    console.log('ENDING.....');
+  }
+}
+
+init();
